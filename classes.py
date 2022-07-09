7ffb3b8839c1316506008bacc260028745315f1d
Raghav Mantri <42713275+raghavm1@users.noreply.github.com>
2022-07-10 01:05:08 +0800

    Complete prelim OOP functionality

--- classes.py
+++ classes.py
@@ -9,6 +9,34 @@ import datetime as dt_time
 import logging
 
 
+# class DoctorList:
+#     """
+#         Class to define a list of doctors
+#     """
+#     list_docs = []
+
+#     file_path = '../data/doctors.csv'
+#     def __init__(self, df_doctors):
+#         self.df_doctors = df_doctors
+#         self.doctors = []
+#         for _, row in df_doctors.iterrows():
+#             self.doctors.append(Doctor(row['id'], row['name']))
+
+
+# class PatientList:
+#     """
+#         Class to define a list of patients
+#     """
+#     Patient_list = []
+
+#     def __init__(self, patients):
+#         self.df_patients = df_patients
+#         self.patients = []
+#         for _, row in df_patients.iterrows():
+#             self.patients.append(
+#                 Patient(row['id'], row['name'], row['birth_date'], row['gender']))
+
+
 class Patient:
     """
         Class created for Patient entity
@@ -56,10 +84,20 @@ class Appointment:
         Class created for defining the appointment relation
     """
 
-    def __init__(self, doctor, patient, time) -> None:
-        self.doctor = doctor
-        self.patient = patient
+    def __init__(self, doctor_id, patient_id, time) -> None:
+        self.doctor_id = doctor_id
+        self.patient_id = patient_id
         self.time = time
+
+    def to_dict(self):
+        """
+            This function aids in the creation of a dataframe
+        """
+        return {
+            'doctor_id': self.doctor_id,
+            'patient_id': self.patient_id,
+            'appointment_datetime': self.time
+        }
 
 
 def read_from_csv(path: str):
@@ -111,6 +149,7 @@ def create_appointment(patient_name, doctor_name, date, time, df_doctor, df_pati
     global df_appointments
     global df_doctors
     global df_patients
+    global appointments
     # Return error on invalid appointment time (i.e. at 3PM or beyond 4PM)
     if(datetime.strptime(time, "%H:%M:%S").time() > datetime.strptime("15:00:00", "%H:%M:%S").time() or datetime.strptime(time, "%H:%M:%S").time() < datetime.strptime("08:00:00", "%H:%M:%S").time()):
         logging.error(
@@ -156,6 +195,7 @@ def create_appointment(patient_name, doctor_name, date, time, df_doctor, df_pati
 
     appointment_id = 'A' + doctor_id + patient_id + \
         datetime_object.strftime('%y%m%d%H')
+    appointments.append(Appointment(doctor_id, patient_id, datetime_object))
     df_appointments = df_appointments.append(
         {'doctor_id': doctor_id, 'patient_id': patient_id, 'appointment_id': appointment_id, 'appointment_datetime': datetime_object}, ignore_index=True)
     return True
@@ -168,25 +208,32 @@ def cancel_appointment(doctor_name=None, patient_name=None, date=None, time=None
     global df_appointments
     global df_doctors
     global df_patients
+    global appointments
+    global doctors
 
+    datetime_string = date + time
+    datetime_object = datetime.strptime(datetime_string, '%Y-%m-%d%H:%M:%S')
     if(doctor_name is None and patient_name is None):
         logging.error("Invalid appointment details")
         return False
 
     if(doctor_name is not None):
         try:
+            doctor_id = [
+                doc.doctor_id for doc in doctors if doc.doctor_name == doctor_name][0]
+            print(doctor_id)
             doctor_id = df_doctors[df_doctors['name']
                                    == doctor_name]['id'].values[0]
         except:
             logging.error(' Doctor not found')
             return False
-        appointments = get_appointments(
+        same_day_apps = get_appointments(
             doctor_name, date, 'doctor')
 
-        if(len(appointments) == 0):
+        if(len(same_day_apps) == 0):
             logging.error("No appointments found")
             return False
-        for _, row in appointments.iterrows():
+        for _, row in same_day_apps.iterrows():
             if(row['appointment_datetime'].strftime('%H:%M:%S') == time):
                 df_appointments = df_appointments[df_appointments['appointment_id']
                                                   != row['appointment_id']]
@@ -200,12 +247,16 @@ def cancel_appointment(doctor_name=None, patient_name=None, date=None, time=None
         except:
             logging.error(' Patient not found')
             return False
-        appointments = get_appointments(
+        same_day_apps = get_appointments(
             patient_name, date, 'patient')
-        if(len(appointments) == 0):
+        if(len(same_day_apps) == 0):
             logging.error("No appointments found")
             return False
-        for _, row in appointments.iterrows():
+
+        appointments = [appointment for appointment in appointments if appointment.time != datetime_object and (
+            (appointment.doctor_id != doctor_id if doctor_id != None else False) or (appointment.patient_id != patient_id if patient_id != None else False))]
+
+        for _, row in same_day_apps.iterrows():
             if(row['appointment_datetime'].strftime('%H:%M:%S') == time):
                 df_appointments = df_appointments[df_appointments['appointment_id']
                                                   != row['appointment_id']]
@@ -220,22 +271,25 @@ if __name__ == '__main__':
 
     patients = []
     doctors = []
+    appointments = []
+
     df_people = pd.DataFrame(
         columns=['patient_id', 'patient_name', 'patient_age', 'patient_gender'])
     df_doctors = pd.DataFrame(columns=['id', 'name'])
 
     for index, row in df.iterrows():
-        patient = Patient(row['patient_id'], row['patient_name'],
-                          row['patient_age'], row['patient_gender'])
-        patients.append(patient)
-        doctor = Doctor(row['doctor_id'], row['doctor_name'])
-        doctors.append(doctor)
+        patients.append(Patient(row['patient_id'], row['patient_name'],
+                                row['patient_age'], row['patient_gender']))
+        doctors.append(Doctor(row['doctor_id'], row['doctor_name']))
+        appointments.append(Appointment(
+            row['doctor_id'], row['patient_id'], row['appointment_datetime']))
+    print([doc.doctor_id for doc in doctors])
     df_patients = pd.DataFrame.from_records(
         [patient.to_dict() for patient in patients])
     df_doctors = pd.DataFrame.from_records(
         [doctor.to_dict() for doctor in doctors])
-    df_appointments = df.drop(['doctor_name', 'patient_name',
-                               'patient_gender', 'patient_age'], axis=1)
+    df_appointments = pd.DataFrame.from_records(
+        [appointment.to_dict() for appointment in appointments])
 
    # print(get_appointments('D1Name', "2018-03-08", 'doctor'))
 
